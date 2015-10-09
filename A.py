7b7120fcdc23ff19fbf38e67a5635821c334ba1b
lev1917 <lev.ryskin@mail.ru>
2015-10-10 01:42:33 +0400

    Update A.py

--- A.py
+++ A.py
@@ -16,3 +16,26 @@
 | 8 3 5 4 5 1 |       |
 +-------------+-------+
 '''
+__author__ = 'student'
+mas = open('input.txt','r')
+V = open('output','w')
+f = mas.readline()
+g = mas.readline()
+f = f.rstrip()
+g = g.rstrip()
+a = list(map(int, g.split()))
+
+for d in range(len(a)):
+    a[d]=int(a[d])
+l=min(a)
+t=0
+o=max(a)
+while o>=l:
+    if a.count(l)==2:
+        t=l
+        break
+    l+=1
+print(t, file=V)
+
+V.close()
+mas.close()
